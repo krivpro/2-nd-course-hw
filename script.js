d0eae0e69acb7e2ce47d718f94cacae9c87acafd
krivpro <krivservice@gmail.com>
2025-01-29 02:46:36 +0300

    Добавил домашку по js

---
+++ script.js
@@ -0,0 +1,43 @@
+let a = 20;
+alert(a);
+
+const year = 2007;
+alert(`Год выпуска первого iPhone - ${year}`);
+
+const name = 'Brendon Eich';
+alert(`${name} - создатель языка JavaScript`);
+
+let b = 10;
+let c = 2;
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+
+let result = 2 ** 5;
+alert(result);
+
+let d = 9;
+let e = 2;
+alert(d % e);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+const user = {
+    name: 'Иван',
+    age: 23,
+    isAdmin: true
+ }
+
+let userName = prompt("Как вас зовут?");
+alert(`Привет, ${userName}!`);
