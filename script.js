424579724bd1d2191cc9ca5e488773851c9e8984
krivpro <krivservice@gmail.com>
2025-02-07 03:01:33 +0300

    Revert "Hw 4"

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-// task-1
-
 let a = 10;
 alert(a);
 a = 20;
 alert(a);
 
-// task-2
-
 const year = 2007;
 alert(`Год выпуска первого iPhone - ${year}`);
 
-// task-3
-
 const name = 'Brendon Eich';
 alert(`${name} - создатель языка JavaScript`);
-
-// task-4
 
 let b = 10;
 let c = 2;
@@ -24,18 +16,12 @@ alert(b - c);
 alert(b * c);
 alert(b / c);
 
-// task-5
-
 let result = 2 ** 5;
 alert(result);
-
-// task-6
 
 let d = 9;
 let e = 2;
 alert(d % e);
-
-// task-7
 
 let num = 1;
 num += 5;
@@ -46,20 +32,14 @@ num++;
 num--;
 alert(num);
 
-// task-8
-
 let age = prompt("Сколько вам лет?");
 alert(age);
-
-// task-9
 
 const user = {
     name: 'Иван',
     age: 23,
     isAdmin: true
-}
-
-// task-10
+ }
 
 let userName = prompt("Как вас зовут?");
 alert(`Привет, ${userName}!`);
